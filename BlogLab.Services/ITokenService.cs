0c2740804dda796be125be2e28dc1bcf5c7fd131
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-10-14 11:45:26 +0530

    Added Jwt service, and thus services completed

---
+++ BlogLab.Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlogLab.Models.Account;
+
+namespace BlogLab.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
