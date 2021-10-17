cd95607ca3d541d8ce32adb981fda01f084c028a
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-10-17 13:45:05 +0530

    Added BlogComment controller, thus finished all controllers

---
+++ BlogLab/Controller/BlogCommentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogLab.Models.BlogComment;
+using BlogLab.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlogLab.Web.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogCommentController : ControllerBase
+    {
+        private readonly IBlogCommentRepository _blogCommentRepository;
+
+        public BlogCommentController(IBlogCommentRepository blogCommentRepository)
+        {
+            _blogCommentRepository = blogCommentRepository;
+        }
+
+        //http://localhost:5000/api/BlogComment [POST] with BlogCommentCreate payload and token in header
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<BlogComment>> Create(BlogCommentCreate blogCommentCreate)
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var createdBlogComment = await _blogCommentRepository.UpsertAsync(blogCommentCreate, applicationUserId);
+            return Ok(createdBlogComment);
+        }
+
+        [HttpGet("{blogId}")]
+        public async Task<ActionResult<List<BlogComment>>> GetAll(int blogId)
+        {
+            var blogComments = await _blogCommentRepository.GetAllByBlogIdAsync(blogId);
+            return Ok(blogComments);
+        }
+
+        [Authorize]
+        [HttpDelete("{blogCommentId}")]
+        public async Task<ActionResult<int>> Delete(int blogCommentId)
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var foundBlogComment = await _blogCommentRepository.GetAsync(blogCommentId);
+            if (foundBlogComment == null) return BadRequest("Comment does not exist!");
+
+            if (foundBlogComment.ApplicationUserId != applicationUserId) return BadRequest("You are not authorized to remove this Blog Comment, since you did not create it!");
+
+            var affectedRows = await _blogCommentRepository.DeleteAsync(blogCommentId);
+            return Ok(affectedRows);
+
+        }
+    }
+}
